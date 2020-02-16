473d860cf3e233f809ae47c6f00bb7a218ce0fcb
Dr AlienTech <rdatch101@gmail.com>
2020-02-16 18:04:01 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const { ipcRenderer } = electron;
+
+const ul = document.querySelector('ul');
+
+const form = document.querySelector('form');
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value;
+
+    ipcRenderer.send('item:add', name);
+};
+
+ipcRenderer.on('item:add', function (e, name) {
+    const li = document.createElement('li');
+    const itemText = document.createTextNode(name);
+    li.appendChild(itemText);
+    ul.appendChild(li);
+});
+
+ipcRenderer.on('item:clear', function (e, name) {
+    ul.innerHTML = "";
+});
+
+ul.addEventListener('dblclick', removeFile);
+
+function removeFile(e) {
+    e.target.remove();
+}
