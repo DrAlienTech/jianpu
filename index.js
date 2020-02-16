4d508e2f15f58d73af430da890911cbe919ff762
Dr AlienTech <rdatch101@gmail.com>
2020-02-16 18:17:55 -0500

    Gitacora saved 1 changed files

--- index.js
+++ index.js
@@ -103,3 +103,33 @@ if (process.env.NODE_ENV !== 'production') {
         ]
     });
 };
+
+const electron = require('electron');
+const { ipcRenderer } = electron;
+
+const ul = document.querySelector('ul');
+
+function submitForm(e) {
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value;
+
+    ipcRenderer.send('item:add', name);
+};
+
+ipcRenderer.on('item:add', function (e, name) {
+    const li = document.createElement('li');
+    const itemText = document.createTextNode(name);
+    li.appendChild(itemText);
+    ul.appendChild(li);
+});
+
+ipcRenderer.on('item:clear', function (e, name) {
+    ul.innerHTML = "";
+});
+
+ul.addEventListener('dblclick', removeFile);
+
+function removeFile(e) {
+    e.target.remove();
+};
