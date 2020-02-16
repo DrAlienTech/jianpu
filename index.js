874eac8a9ecd0514dad87206ebd9a31193337e93
Dr AlienTech <rdatch101@gmail.com>
2020-02-16 09:15:07 -0500

    Gitacora saved 1 changed files

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const mainMenuTemplate = [
             {
                 label: 'Delete Files',
                 click() {
-                    mainWindow.webContents.send()
+                    mainWindow.webContents.send('item:clear')
                 }
             },
             {
