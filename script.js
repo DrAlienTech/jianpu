885828ad23f7a1c77b38c4e2837523dfe7906566
Dr AlienTech <rdatch101@gmail.com>
2020-02-16 19:15:12 -0500

    Gitacora saved 1 changed files

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const electron = require('electron');
-const { ipcRenderer } = electron;
+// const electron = require('electron');
+// const { ipcRenderer } = electron;
 
-const ul = document.querySelector('ul');
+// const ul = document.querySelector('ul');
 
-function submitForm(e) {
-    e.preventDefault();
+// function submitForm(e) {
+//     e.preventDefault();
 
-    const name = document.querySelector('#name').value;
+//     const name = document.querySelector('#name').value;
 
-    ipcRenderer.send('item:add', name);
-};
+//     ipcRenderer.send('item:add', name);
+// };
 
-ipcRenderer.on('item:add', function (e, name) {
-    const li = document.createElement('li');
-    const itemText = document.createTextNode(name);
-    li.appendChild(itemText);
-    ul.appendChild(li);
-});
+// ipcRenderer.on('item:add', function (e, name) {
+//     const li = document.createElement('li');
+//     const itemText = document.createTextNode(name);
+//     li.appendChild(itemText);
+//     ul.appendChild(li);
+// });
 
-ipcRenderer.on('item:clear', function (e, name) {
-    ul.innerHTML = "";
-});
+// ipcRenderer.on('item:clear', function (e, name) {
+//     ul.innerHTML = "";
+// });
 
-ul.addEventListener('dblclick', removeFile);
+// ul.addEventListener('dblclick', removeFile);
 
-function removeFile(e) {
-    e.target.remove();
-};
+// function removeFile(e) {
+//     e.target.remove();
+// };
